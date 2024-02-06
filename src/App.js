ee6897d0e6fbe9b644b65f7a39f730f97f6670d4
Ted <shinsuengju1@gmail.com>
2024-02-06 12:45:41 +0900

    #7.2 Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import { useState, useEffect } from 'react';
 function App() {
-  const [toDo, setToDo] = useState('');
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === '') {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo('');
-  };
-  const onDeleteBtn = (index) => {
-    setToDos((curToDos) =>
-      curToDos.filter((_, curIndex) => curIndex !== index)
-    );
-  };
-
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch('https://api.coinpaprika.com/v1/tickers')
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type='text'
-          placeholder='Write your to do...'
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
+      <h1>The Coins! {loading ? null : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item} <button onClick={() => onDeleteBtn(index)}>❌</button>
+        {coins.map((coin) => (
+          <li>
+            {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
           </li>
         ))}
       </ul>
