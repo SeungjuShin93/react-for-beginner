bf4117eb472e61b883da44d0e99814ff985d3309
Ted <shinsuengju1@gmail.com>
2024-02-07 15:03:33 +0900

    react-router-dom 6.22 최신버전 업데이트 코드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './routes/Home';
 import Detail from './routes/Detail';
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path='/hello'>
-          <h1>hello</h1>
-        </Route>
-        <Route path='/movie'>
-          <Detail />
-        </Route>
-        <Route path='/'>
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
-}
+const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/movie',
+      element: <Detail />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
+};
 
 export default App;
